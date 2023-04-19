d8c6eab32206576a730eacc5ab73cee9112b5193
AnhelinaKyrylova <xkyrylova@stuba.sk>
2023-04-19 09:24:00 +0200

    Initial commit

---
+++ src/main/java/com/example/feioopcv9_/FeiOopCv9Application.java
@@ -0,0 +1,13 @@
+package com.example.feioopcv9_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiOopCv9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeiOopCv9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/feioopcv9_/FeiOopCv9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feioopcv9_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiOopCv9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
